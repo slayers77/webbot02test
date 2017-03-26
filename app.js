b40f08613344e8d9efedc0b5728f7f01d5d6e298
bobby404 <kspark@taihoinst.com>
2017-03-26 17:41:49 +0900

    [2017.03.26] kspark -app.js 수정

--- app.js
+++ app.js
@@ -17,12 +17,38 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-var bot = new builder.UniversalBot(connector);
+//var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
+
+var bot = new builder.UniversalBot(connector,[
+
+    function (session) {
+        builder.Prompts.text("Hello World from " + botenv);
+        builder.Prompts.choice(session, "Hello.... What's your preferred language?", 'English|Korean');
+    },
+    function (session, results) {
+
+        var locale;
+        switch (results.response.entity) {
+            case 'English':
+                locale = 'en';
+            case 'Korean':
+                locale = 'kr';
+                break;
+        }
+        //session.endDialog(result.response.entity);
+    },
+    function (session, result) {
+        session.userData.language = result.response.entity;
+        builder.Prompts.text("Your preferred language is now %s.", session.userData.language);
+    }
+
+]);
+
 
 /*
 bot.dialog('/', function (session) {
@@ -32,56 +58,56 @@ bot.dialog('/', function (session) {
 });
 */    
     
-bot.dialog('/', [
-    function (session) {
-        session.send("Hello World from " + botenv);
-        session.beginDialog('/localePicker');
-        
-        
-    },
-    function (session, results) {
-        session.send('Hello %s!', results.response);
-    }
-]);
-bot.dialog('/askName', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    function (session, results) {
-        session.endDialog(results);
-    }
-]);
+//bot.dialog('/', [
+//    function (session) {
+//        session.send("Hello World from " + botenv);
+//        session.beginDialog('/localePicker');
+
+//    },
+//    function (session, results) {
+//        session.send("Your preferred language is now %s.", results.response.entity);
+//        session.beginDialog('/askName');
+//        //session.send('Hello %s!', results.response);
+//    }
+//]);
+//bot.dialog('/askName', [
+//    function (session) {
+//        builder.Prompts.text(session, 'What is your name?');
+//    },
+//    function (session, results) {
+//        session.send('Hello %s!', results.response);
+//        session.endDialog(results);
+//    }
+//]);
 
 
-bot.dialog('/localePicker', [
-    function (session) {
-        // Prompt the user to select their preferred locale
-        builder.Prompts.choice(session, "What's your preferred language?", 'English|Espa?ol|Italiano');
-    },
-    function (session, results) {
-        // Update preferred locale
-        var locale;
-        switch (results.response.entity) {
-            case 'English':
-                locale = 'en';
-            case 'Espa?ol':
-                locale = 'es';
-            case 'Italiano':
-                locale = 'it';
-                break;
-        }
-        session.preferredLocale(locale, function (err) {
-            if (!err) {
-                // Locale files loaded
-                session.endDialog("Your preferred language is now %s.", results.response.entity);
-            } else {
-                // Problem loading the selected locale
-                session.error(err);
-            }
-        });
-        session.beginDialog('/askName');
-    }
-]);
+//bot.dialog('/localePicker', [
+//    function (session) {
+//        // Prompt the user to select their preferred locale
+//        builder.Prompts.choice(session, "What's your preferred language?", 'English|Korean');
+//    },
+//    function (session, results) {
+//        // Update preferred locale
+//        var locale;
+//        switch (results.response.entity) {
+//            case 'English':
+//                locale = 'en';
+//            case 'Korean':
+//                locale = 'kr';
+//                break;
+//        }
+//        session.endDialog(result.response.entity);
+//        //session.preferredLocale(locale, function (err) {
+//        //    if (!err) {
+//        //        // Locale files loaded
+//        //        session.endDialog("Your preferred language is now %s.", results.response.entity);
+//        //    } else {
+//        //        // Problem loading the selected locale
+//        //        session.error(err);
+//        //    }
+//        //});
+//    }
+//]);
 
 
 var app = express();
