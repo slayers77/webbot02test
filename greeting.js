2764104405abd1a49684472a1ce0b1fd0daa77d0
bobby404 <kspark@taihoinst.com>
2017-03-28 13:26:57 +0900

    [20170328] kspark app.js change

--- greeting.js
+++ greeting.js
@@ -7,14 +7,14 @@ exports.create = function (bot) {
     function (session) {
 
         //session.send("HI..");
-        builder.Prompts.choice(session, "Hi.. Choose Your Language : ", 'English|Korean', { listStyle: builder.ListStyle.button });
+        builder.Prompts.choice(session, "Hi.. Choose or Typing Your Language : ", 'English|Korean', { listStyle: builder.ListStyle.button });
 
     },
 
     function (session, results) {
         session.preferredLocale(results.response.entity, function (err) {
             if (!err) {
-                session.send("Your Choice Language %s.", results.response.entity);
+                //session.send("Your Choice Language %s.", results.response.entity);
                 session.userData.language = results.response.entity;
                 if (results.response.entity == "English") {
                     session.send("Hi!! I`m Hyundai Motors ChatBot  Grandizer!!");
@@ -51,18 +51,30 @@ exports.create = function (bot) {
 
             session.send('Your AgeGroup :  %s!', results.response.entity);
             //session.userData.age = results.response.entity;
-            ession.send("Your Choice Language : " + session.userData.language + " Your Name : " + session.userData.name + " Your Age : " + results.response.entity);
+            //ession.send("Your Choice Language : " + session.userData.language + " Your Name : " + session.userData.name + " Your Age : " + results.response.entity);
             session.send("OK.. Let`s Go Grandizer..!!" + session.userData.name);
-            builder.Prompts.choice(session, 'What do you want menu?', 'testDrive|Design|Convenience|Price', { listStyle: builder.ListStyle.button });
+            builder.Prompts.choice(session, 'What do you want menu? choice or typing!!', 'testDrive|Design|Convenience|Price', { listStyle: builder.ListStyle.button });
 
         } else if (session.userData.language == 'Korean') {
 
             session.send('당신의 연령대는 : %s!', results.response.entity);
             //session.userData.menu = results.response.entity;
             //session.send("당신이 선택한 언어 : %s  당신의 이름 : %s  당신의 연령대 : %s", session.userData.language, session.userData.name, session.userData.age);
-            session.send("당신이 선택한 언어 : " + session.userData.language + "  당신의 이름 : " + session.userData.name + "  당신의 연령대 : " + results.response.entity);
+            //session.send("당신이 선택한 언어 : " + session.userData.language + "  당신의 이름 : " + session.userData.name + "  당신의 연령대 : " + results.response.entity);
             session.send("OK.. 그랜다이저를 시작해볼까요..!! %s 님", session.userData.name);
-            builder.Prompts.choice(session, '원하시는 메뉴를 선택하세요?', '시승|디자인|편의사항|가격', { listStyle: builder.ListStyle.button });
+            builder.Prompts.choice(session, '원하시는 메뉴를 선택하세요? 선택하시거나 질문해주세요!!', '시승|디자인|편의사항|가격', { listStyle: builder.ListStyle.button });
+
+        }
+    },
+    function (session, results) {
+
+        if (session.userData.language == 'English') {
+
+            session.send('Your Select menu :  %s!', results.response.entity);
+
+        } else if (session.userData.language == 'Korean') {
+
+            session.send('당신의 선택 메뉴 : %s!', results.response.entity);
 
         }
     }
