431542f6ad9a0a3033fe084c02354a8fdc2dcc38
bobby404 <kspark@taihoinst.com>
2017-03-28 10:48:14 +0900

    [20170328] kspark app.js change

--- app.js
+++ app.js
@@ -31,141 +31,174 @@ server.post('/api/messages', connector.listen());
 
 bot.dialog('/', [
 
-    function (session,args,next) {
+    function (session) {
 
         //session.send("HI..");
-        session.beginDialog('choiceLanguage');
+        builder.Prompts.choice(session, "Hi.. Choose Your Language : ", 'English|Korean', { listStyle: builder.ListStyle.button });
 
     },
+
     function (session, results) {
-
-        if (session.userData.language == 'English') {
-
-            session.send("Your Choice Language : " + session.userData.language + "\n Your Name : " + session.userData.name + "\n Your Age : " + session.userData.age);
-            session.send("OK.. Let`s Go Grandizer..!!" + session.userData.name);
-            session.beginDialog('selectEngMenu');
-            
-
-
-        } else if (session.userData.language == 'Korean') {
-
-            //session.send("당신이 선택한 언어 : %s" + session.userData.language + " 당신의 이름 : " + session.userData.name + " 당신의 연령대 : " + session.userData.age);
-            session.send("당신이 선택한 언어 : %s  당신의 이름 : %s  당신의 연령대 : %s", session.userData.language, session.userData.name ,session.userData.age);
-            session.send("OK.. 그랜다이저를 시작해볼까요..!! %s 님", session.userData.name);
-            console.log('hhhhhhhhhhhhhh');
-            session.beginDialog('selectKorMenu');
-            
-
-        }
-
-    }
-]);
-
-
-bot.dialog('choiceLanguage', [
-         function (session) {
-        // Prompt the user to select their preferred locale 
-             builder.Prompts.choice(session, "Hi.. Choose Your Language : ", 'English|Korean', { listStyle: builder.ListStyle.button });
-             //builder.Prompts.choice(session, "Hi.. Choose Your Language : ", 'English|Korean', { listStyle: builder.ListStyle.inline });
-             //builder.Prompts.choice(session, "Hi.. Choose Your Language : ", 'English|Korean', { listStyle: builder.ListStyle.list });
-        
-    },
-         function (session, results) {
-            session.preferredLocale(results.response.entity, function (err) {
+        session.preferredLocale(results.response.entity, function (err) {
             if (!err) {
                 session.send("Your Choice Language %s.", results.response.entity);
                 session.userData.language = results.response.entity;
                 if (results.response.entity == "English") {
                     session.send("Hi!! I`m Hyundai Motors ChatBot  Grandizer!!");
-                    session.beginDialog('/askNameEng');
+                    builder.Prompts.text(session, 'What is your name?');
                 }
                 else if (results.response.entity == "Korean") {
                     session.send("안녕!! 난 현대자동차 챗봇 그랜다이저야 !!");
-                    session.beginDialog('/askNameKor');
+                    builder.Prompts.text(session, '당신의 이름은?');
                 }
             } else {
-                    session.error(err);
+                session.error(err);
             }
-        }); 
-     } 
-]); 
-
-
-bot.dialog('/askNameEng', [
-    function (session) {
-        builder.Prompts.text(session, 'What is your name?');
+        });
     },
     function (session, results) {
-        session.send('Hello %s!', results.response);
-        session.userData.name = results.response;
-        session.beginDialog('/askAgeEng');
-        //session.endDialog(results);
+
+        if (session.userData.language == 'English') {
+
+            session.send('Hello %s!', results.response);
+            session.userData.name = results.response;
+            builder.Prompts.choice(session, 'What is your Age Group?', '10~20 AgeGroup|30 AgeGroup|40 AgeGroup|50 AgeGroup| 60 Over AgeGroup', { listStyle: builder.ListStyle.button });
+
+        } else if (session.userData.language == 'Korean') {
+
+            session.send('안녕 %s!', results.response);
+            session.userData.name = results.response;
+            builder.Prompts.choice(session, '당신의 연령대는?', '10~20대|30대|40대|50대|60대이상', { listStyle: builder.ListStyle.button });
+
+        }
+    },
+    function (session, results) {
+
+        if (session.userData.language == 'English') {
+
+            session.send('Your AgeGroup :  %s!', results.response.entity);
+            session.userData.age = results.response.entity;
+            ession.send("Your Choice Language : " + session.userData.language + "\n Your Name : " + session.userData.name + "\n Your Age : " + session.userData.age);
+            session.send("OK.. Let`s Go Grandizer..!!" + session.userData.name);
+            builder.Prompts.choice(session, 'What do you want menu?', 'testDrive|Design|Convenience|Price', { listStyle: builder.ListStyle.button });
+
+        } else if (session.userData.language == 'Korean') {
+
+            session.send('Your Choice Menu : %s!', results.response.entity);
+            session.userData.menu = results.response.entity;
+            session.send("당신이 선택한 언어 : %s  당신의 이름 : %s  당신의 연령대 : %s", session.userData.language, session.userData.name, session.userData.age);
+            session.send("OK.. 그랜다이저를 시작해볼까요..!! %s 님", session.userData.name);
+            builder.Prompts.choice(session, '원하시는 메뉴를 선택하세요?', '시승|디자인|편의사항|가격', { listStyle: builder.ListStyle.button });
+
+        }
     }
 ]);
 
 
-bot.dialog('/askNameKor', [
-    function (session) {
-        builder.Prompts.text(session, '당신의 이름은?');
-    },
-    function (session, results) {
-        session.send('안녕 %s!', results.response);
-        session.userData.name = results.response;
-        session.beginDialog('/askAgeKor');
-        //session.endDialog(results);
-    }
-]);
-
-bot.dialog('/askAgeEng', [
-    function (session) {
-        //builder.Prompts.text(session, 'What is your Age?');
-        builder.Prompts.choice(session, 'What is your Age Group?', '10~20 AgeGroup|30 AgeGroup|40 AgeGroup|50 AgeGroup| 60 Over AgeGroup', { listStyle: builder.ListStyle.button });
-    },
-    function (session, results) {
-        session.send('Your AgeGroup :  %s!', results.response.entity);
-        session.userData.age = results.response.entity;
-        //session.endDialog(results);
-    }
-]);
-
-
-bot.dialog('/askAgeKor', [
-    function (session) {
-        builder.Prompts.choice(session, '당신의 연령대는?', '10~20대|30대|40대|50대|60대이상', { listStyle: builder.ListStyle.button });
-    },
-    function (session, results) {
-        session.send('당신의 연령대는 : %s!', results.response.entity);
-        session.userData.age = results.response.entity;
-        //session.endDialog(results);
-    }
-]);
+//bot.dialog('choiceLanguage', [
+//         function (session) {
+//        // Prompt the user to select their preferred locale 
+//             builder.Prompts.choice(session, "Hi.. Choose Your Language : ", 'English|Korean', { listStyle: builder.ListStyle.button });
+//             //builder.Prompts.choice(session, "Hi.. Choose Your Language : ", 'English|Korean', { listStyle: builder.ListStyle.inline });
+//             //builder.Prompts.choice(session, "Hi.. Choose Your Language : ", 'English|Korean', { listStyle: builder.ListStyle.list });
+        
+//    },
+//         function (session, results) {
+//            session.preferredLocale(results.response.entity, function (err) {
+//            if (!err) {
+//                session.send("Your Choice Language %s.", results.response.entity);
+//                session.userData.language = results.response.entity;
+//                if (results.response.entity == "English") {
+//                    session.send("Hi!! I`m Hyundai Motors ChatBot  Grandizer!!");
+//                    session.beginDialog('/askNameEng');
+//                }
+//                else if (results.response.entity == "Korean") {
+//                    session.send("안녕!! 난 현대자동차 챗봇 그랜다이저야 !!");
+//                    session.beginDialog('/askNameKor');
+//                }
+//            } else {
+//                    session.error(err);
+//            }
+//        }); 
+//     } 
+//]); 
 
 
+//bot.dialog('/askNameEng', [
+//    function (session) {
+//        builder.Prompts.text(session, 'What is your name?');
+//    },
+//    function (session, results) {
+//        session.send('Hello %s!', results.response);
+//        session.userData.name = results.response;
+//        session.beginDialog('/askAgeEng');
+//        //session.endDialog(results);
+//    }
+//]);
+
+
+//bot.dialog('/askNameKor', [
+//    function (session) {
+//        builder.Prompts.text(session, '당신의 이름은?');
+//    },
+//    function (session, results) {
+//        session.send('안녕 %s!', results.response);
+//        session.userData.name = results.response;
+//        session.beginDialog('/askAgeKor');
+//        //session.endDialog(results);
+//    }
+//]);
+
+//bot.dialog('/askAgeEng', [
+//    function (session) {
+//        //builder.Prompts.text(session, 'What is your Age?');
+//        builder.Prompts.choice(session, 'What is your Age Group?', '10~20 AgeGroup|30 AgeGroup|40 AgeGroup|50 AgeGroup| 60 Over AgeGroup', { listStyle: builder.ListStyle.button });
+//    },
+//    function (session, results) {
+//        session.send('Your AgeGroup :  %s!', results.response.entity);
+//        session.userData.age = results.response.entity;
+//        //session.endDialog(results);
+//    }
+//]);
+
+
+//bot.dialog('/askAgeKor', [
+//    function (session) {
+//        builder.Prompts.choice(session, '당신의 연령대는?', '10~20대|30대|40대|50대|60대이상', { listStyle: builder.ListStyle.button });
+//    },
+//    function (session, results) {
+//        session.send('당신의 연령대는 : %s!', results.response.entity);
+//        session.userData.age = results.response.entity;
+//        //session.endDialog(results);
+//    }
+//]);
 
 
 
-bot.dialog('selectKorMenu', [
-    function (session) {
-        console.log('selectKorMenu');
-        builder.Prompts.choice(session, '원하시는 메뉴를 선택하세요?', '시승|디자인|편의사항|가격', { listStyle: builder.ListStyle.button });
-    },
-    function (session, results) {
-        session.send('당신이 선택한 메뉴는 : %s!', results.response.entity);
-        session.userData.menu = results.response.entity;
-        //luisKorServer.beginDialog(session);
-    }
-]);
 
-bot.dialog('selectEngMenu', [
-    function (session) {
-        builder.Prompts.choice(session, 'What do you want menu?', 'testDrive|Design|Convenience|Price', { listStyle: builder.ListStyle.button });
-    },
-    function (session, results) {
-        session.send('Your Choice Menu : %s!', results.response.entity);
-        session.userData.menu = results.response.entity;
-        //luisEngServer.beginDialog(session); 
-    }
-]);
+
+//bot.dialog('selectKorMenu', [
+//    function (session) {
+//        console.log('selectKorMenu');
+//        builder.Prompts.choice(session, '원하시는 메뉴를 선택하세요?', '시승|디자인|편의사항|가격', { listStyle: builder.ListStyle.button });
+//    },
+//    function (session, results) {
+//        session.send('당신이 선택한 메뉴는 : %s!', results.response.entity);
+//        session.userData.menu = results.response.entity;
+//        //luisKorServer.beginDialog(session);
+//    }
+//]);
+
+//bot.dialog('selectEngMenu', [
+//    function (session) {
+//        builder.Prompts.choice(session, 'What do you want menu?', 'testDrive|Design|Convenience|Price', { listStyle: builder.ListStyle.button });
+//    },
+//    function (session, results) {
+//        session.send('Your Choice Menu : %s!', results.response.entity);
+//        session.userData.menu = results.response.entity;
+//        //luisEngServer.beginDialog(session); 
+//    }
+//]);
 
 
 
