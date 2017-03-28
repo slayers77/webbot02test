81748e2382b3942cad4b4865eb6346a5efd55530
bobby404 <kspark@taihoinst.com>
2017-03-28 17:46:31 +0900

    [20170328] kspark app.js change

--- app.js
+++ app.js
@@ -24,19 +24,28 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-
+var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
+
+
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/6393ebda-613e-477e-bade-92330e2e496d?subscription-key=7489b95cf3fb4797939ea70ce94a4b11';
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] }); 
+
+
+
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
-var bot = new builder.UniversalBot(connector, [
+//bot.dialog('/', dialog);
+//var bot = new builder.UniversalBot(connector, [
 
-    function (session) {
-        greeting.beginDialog(session);
-    }
+//    function (session) {
+//        greeting.beginDialog(session);
+//    }
 
-]);
+//]);
 greeting.create(bot);
 
 
