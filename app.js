7c8d9f18d8a456b85311be4d63994a00b0e3bb26
jangsangryeon <srjang@taihoinst.com>
2017-04-04 19:17:42 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ var restify = require('restify');
 var builder = require('botbuilder');
 var express = require('express');
 var request = require('request');
+
+//인사
 var greeting = require('./greeting');
+//시승
+var testDriveKor = require('./testDrive/kor/testDriveKor');
+//디자인
+var designKor = require('./design/kor/designKor');
+//편의사항
+var convenienceKor = require('./convenience/kor/convenienceKor');
+//가격
+var priceKor = require('./price/kor/priceKor');
+
 
 
 //=========================================================
@@ -12,25 +23,33 @@ var greeting = require('./greeting');
 // Setup Restify Server
 var server = restify.createServer();
 var botenv = process.env.BOT_ENV;
+
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s (%s)', server.name, server.url, botenv);
 });
+
 
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
-    //appId: "98ee3e87-b025-4e50-a7b3-1df9e902dacc",
-    //appPassword: "aKLgB9zVeiCPRBPObwAvwEr"
+
 });
+
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-var bot = new builder.UniversalBot(connector);
-greeting.create(bot);
-server.post('/api/messages', connector.listen());
+//분기작업테스트 
+greeting.create(bot);           // 인사
+testDriveKor.create(bot);       // 시승
+designKor.create(bot);          // 디자인
+convenienceKor.create(bot);     // 편의사항
+priceKor.create(bot);           // 가격
+
 
 var app = express();
 var fs = require('fs');
