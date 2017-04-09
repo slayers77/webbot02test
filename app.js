dbf0ffee12a0457d97145b4e09ebe4cc0e5ddf63
kimhyunsuk <hskim@taihoinst.com>
2017-04-09 13:59:50 +0900

    openning mesage change

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ var testDriveKor = require('./testDrive/kor/testDriveKor');
 var designKor = require('./design/kor/designKor');
 var convenienceKor = require('./convenience/kor/convenienceKor');
 var priceKor = require('./price/kor/priceKor');
+var query = require('./config/query');
 
-
+global.sample;
 
 //=========================================================
 // Bot Setup
@@ -19,7 +20,7 @@ var priceKor = require('./price/kor/priceKor');
 var server = restify.createServer();
 var botenv = process.env.BOT_ENV;
 
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(3978, function () {
    console.log('%s listening to %s (%s)', server.name, server.url, botenv);
 });
 
@@ -35,7 +36,6 @@ server.post('/api/messages', connector.listen());
 
 var bot = new builder.UniversalBot(connector);
 
-
 bot.on('conversationUpdate', function (message) {
 	if (message.membersAdded && message.membersAdded.length > 0) {
 		var membersAdded = message.membersAdded
@@ -45,7 +45,7 @@ bot.on('conversationUpdate', function (message) {
                 })
                 .join(', ');
 		if (membersAdded != 'Bot') {
-			bot.beginDialog(message.address, '/init');
+            bot.beginDialog(message.address, '/init');
 		}
 	}
 });
@@ -54,34 +54,55 @@ bot.on('conversationUpdate', function (message) {
 
 bot.dialog('/init', [
     function (session) {
-        builder.Prompts.choice(session, "What's your preferred language?", '한국어|English');
+        var msg = new builder.Message(session)
+                .attachments([
+                    new builder.AudioCard(session)
+                        .title('그랜다이저')
+                        .subtitle('Grandizer')
+                        .text('안녕하세요. 저는 현대자동차의 그랜저 ig를 소개하는 그랜다이저예요. \n\nHi. My name is Grandizer.')
+                        .image(builder.CardImage.create(session, 'http://webbot02.azurewebsites.net/hyundai/images/openning.png'))
+                        .media([
+                            { url: 'http://webbot02.azurewebsites.net/openning.wav' }
+                        ])
+                        .buttons([
+                            builder.CardAction.imBack(session, "한국어", "한국어"),
+                            builder.CardAction.imBack(session, "English", "English")
+                        ])
+                ]);
+                builder.Prompts.choice(session, msg, '한국어|English');
     },
     function (session, results) {
-		var locale;
+        var locale;
         switch (results.response.entity) {
             case '한국어':
                 locale = 'ko';
-				break;
+                break;
             case 'English':
                 locale = 'en';
                 break;
         }
-		session.preferredLocale(locale, function (err) {
+        session.preferredLocale(locale, function (err) {
             if (err) {
                 session.error(err);
             }
         });
-		var options = session.localizer.gettext(session.preferredLocale(), "name");
-		var msg = new builder.Message(session)
-		.attachments([
-			new builder.HeroCard(session)
-				.title(options)
-				.images([
-					builder.CardImage.create(session, "http://webbot02.azurewebsites.net/hyundai/images/Grandeur_main.png")
-				])
-		]);
-		session.send(msg);
-		session.endDialog();
+        var options = session.localizer.gettext(session.preferredLocale(), "name");
+        var msg = new builder.Message(session)
+            .attachments([
+                new builder.HeroCard(session)
+                    .title(options)
+                    .text("그랜저 ig 가격이 궁금해서 오신거죠? 어떤 정보를 알고 싶으세요?")
+                        .buttons([
+                            builder.CardAction.imBack(session, "가격 보여줘", "가격"),
+                            builder.CardAction.imBack(session, "디자인 보여줘", "디자인"),
+                            builder.CardAction.imBack(session, "편의사항 보여줘", "편의사항"),
+                            builder.CardAction.imBack(session, "시승 보여줘", "시승")
+                        ])
+            ]);
+
+        builder.Prompts.choice(session, msg, '가격|디자인|편의사항|시승');
+        session.endDialog();
+        console.log("user insert : " + session.message.text);
     }
 ]);
 
