112a67703cf72cf6fee0fa4087072927f4c40c79
kimhyunsuk <hskim@taihoinst.com>
2017-04-09 15:21:19 +0900

    error fix 2

--- app.js
+++ app.js
@@ -36,83 +36,11 @@ server.post('/api/messages', connector.listen());
 
 var bot = new builder.UniversalBot(connector);
 
-bot.on('conversationUpdate', function (message) {
-	if (message.membersAdded && message.membersAdded.length > 0) {
-		var membersAdded = message.membersAdded
-                .map(function (m) {
-                    var isSelf = m.id === message.address.bot.id;
-                    return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
-                })
-                .join(', ');
-		if (membersAdded != 'Bot') {
-            bot.beginDialog(message.address, '/init');
-		}
-	}
-});
-
-//초기 출력 화면
-
-bot.dialog('/init', [
-    function (session) {
-        var msg = new builder.Message(session)
-                .attachments([
-                    new builder.AudioCard(session)
-                        .title('그랜다이저')
-                        .subtitle('Grandizer')
-                        .text('안녕하세요. 저는 현대자동차의 그랜저 ig를 소개하는 그랜다이저예요. \n\nHi. My name is Grandizer.')
-                        .image(builder.CardImage.create(session, 'http://webbot02.azurewebsites.net/hyundai/images/openning.png'))
-                        .media([
-                            { url: 'http://webbot02.azurewebsites.net/openning.wav' }
-                        ])
-                        .buttons([
-                            builder.CardAction.imBack(session, "한국어", "한국어"),
-                            builder.CardAction.imBack(session, "English", "English")
-                        ])
-                ]);
-                builder.Prompts.choice(session, msg, '한국어|English');
-    },
-    function (session, results) {
-        var locale;
-        switch (results.response.entity) {
-            case '한국어':
-                locale = 'ko';
-                break;
-            case 'English':
-                locale = 'en';
-                break;
-        }
-        session.preferredLocale(locale, function (err) {
-            if (err) {
-                session.error(err);
-            }
-        });
-        var options = session.localizer.gettext(session.preferredLocale(), "name");
-        var msg = new builder.Message(session)
-            .attachments([
-                new builder.HeroCard(session)
-                    .title(options)
-                    .text(session.localizer.gettext(session.preferredLocale(), "welcomemessage"))
-                        .buttons([
-                            builder.CardAction.imBack(session, "가격 보여줘", "가격"),
-                            builder.CardAction.imBack(session, "디자인 보여줘", "디자인"),
-                            builder.CardAction.imBack(session, "편의사항 보여줘", "편의사항"),
-                            builder.CardAction.imBack(session, "시승 보여줘", "시승")
-                        ])
-            ]);
-
-        builder.Prompts.choice(session, msg, '가격|디자인|편의사항|시승');
-        session.endDialog();
-        
-        greeting.create(bot, session.preferredLocale());           // 인사
-        testDriveKor.create(bot);       // 시승
-        designKor.create(bot);          // 디자인
-        convenienceKor.create(bot);     // 편의사항
-        priceKor.create(bot);           // 가격
-        console.log("user insert : " + session.message.text);
-    }
-]);
-
-
+greeting.create(bot);           // 인사
+testDriveKor.create(bot);       // 시승
+designKor.create(bot);          // 디자인
+convenienceKor.create(bot);     // 편의사항
+priceKor.create(bot);           // 가격
 //=========================================================
 // Bots Dialogs
 //=========================================================
