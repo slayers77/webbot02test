b503313e597158ae78dea85ff6f7f52226ee3032
songhj <songhj@SONGHJ-PC>
2017-04-12 20:41:05 +0900

    commit

--- greeting.js
+++ greeting.js
@@ -78,7 +78,7 @@ function create(bot) {                                                  // funct
                             .title('그랜다이저')
                             .autostart(true)
                             .subtitle('Grandizer')
-                            .text("안녕하세요. 저는 현대자동차의 그랜저 ig를 소개하는 그랜다이저예요. \n\n 대화중 언제든지'그랜다이저' 라고 입력하면 초기 화면으로 돌아가요.            \n\nHi. My name is Grandizer. \n\n At any time, type 'Grandizer' to return to the initial screen. ")
+                            .text("안녕하세요. 저는 현대자동차의 그랜저 ig를 소개하는 그랜다이저예요. \n\n 대화중 언제든지'그랜다이저' 라고 입력하면 초기 화면으로 돌아가요.            \n\n Hi. My name is Grandizer. \n\n At any time, type 'Grandizer' to return to the initial screen. ")
                             .image(builder.CardImage.create(session, img_path + "/images/img_car01.jpg"))
                             //.images([
                             //    builder.CardImage.create(session, img_path + "/images/img_car01.jpg")
