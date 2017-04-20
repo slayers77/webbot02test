3c5b93dfd8edde358a6e4fc7b37da9a6df95ccf3
Sinook Kang <sokang337@gmail.com>
2017-04-20 09:34:32 +0900

    ddd

--- price/kor/priceKor.js
+++ price/kor/priceKor.js
@@ -725,7 +725,7 @@ function create(bot) {
                                 template_type: "receipt",
                                 recipient_name: "Stephane Crozatier",
                                 order_number: "12345678902",
-                                currency: "USD",
+                                currency: "KRW",
                                 payment_method: "Visa 2345",
                                 order_url: "http://petersapparel.parseapp.com/order?order_id=123456",
                                 timestamp: "1428444852",
@@ -734,16 +734,16 @@ function create(bot) {
                                         title: "Classic White T-Shirt",
                                         subtitle: "100% Soft and Luxurious Cotton",
                                         quantity: 2,
-                                        price: 50,
-                                        currency: "USD",
+                                        price:1000000,
+                                        currency: "KRW",
                                         image_url: "http://petersapparel.parseapp.com/img/whiteshirt.png"
                                     },
                                     {
                                         title: "Classic Gray T-Shirt",
                                         subtitle: "100% Soft and Luxurious Cotton",
                                         quantity: 1,
-                                        price: 25,
-                                        currency: "USD",
+                                        price: 1250000,
+                                        currency: "KRW",
                                         image_url: "http://petersapparel.parseapp.com/img/grayshirt.png"
                                     }
                                 ],
@@ -756,7 +756,7 @@ function create(bot) {
                                     country: "US"
                                 },
                                 summary: {
-                                    subtotal: 75.00,
+                                    subtotal: elements[0].price + elements[1].price,
                                     shipping_cost: 4.95,
                                     total_tax: 6.19,
                                     total_cost: 56.14
