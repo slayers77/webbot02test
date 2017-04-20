bd32099619631bbd22acf9f49fd31f08abc99480
Sinook Kang <sokang337@gmail.com>
2017-04-20 09:43:53 +0900

    ssdf

--- price/kor/priceKor.js
+++ price/kor/priceKor.js
@@ -703,6 +703,77 @@ function create(bot) {
                     builder.ReceiptItem.create(session, number_format(searchData[8][3]) + session.localizer.gettext(query.kor_en_Checker(session.message.text), "priceReciptCurrencyUnit"), searchData[8][2])];
             }
 
+
+            var msg;
+            if (session.message.address.channelId == "facebook"){
+                msg = new builder.Message(session)
+                    .sourceEvent({
+                        facebook: {
+                            attachment: {
+                                type: "template",
+                                payload: {
+                                    template_type: "receipt",
+                                    recipient_name: "Stephane Crozatier",
+                                    order_number: "12345678902",
+                                    currency: "KRW",
+                                    payment_method: "Visa 2345",
+                                    order_url: "http://petersapparel.parseapp.com/order?order_id=123456",
+                                    timestamp: "1428444852",
+                                    elements: [
+                                        {
+                                            title: "Classic White T-Shirt",
+                                            subtitle: "100% Soft and Luxurious Cotton",
+                                            quantity: 2,
+                                            price: 1000000,
+                                            currency: "KRW",
+                                            image_url: "http://petersapparel.parseapp.com/img/whiteshirt.png"
+                                        },
+                                        {
+                                            title: "Classic Gray T-Shirt",
+                                            subtitle: "100% Soft and Luxurious Cotton",
+                                            quantity: 1,
+                                            price: 1250000,
+                                            currency: "KRW",
+                                            image_url: "http://petersapparel.parseapp.com/img/grayshirt.png"
+                                        }
+                                    ],
+                                    address: {
+                                        street_1: "1 Hacker Way",
+                                        street_2: "",
+                                        city: "Menlo Park",
+                                        postal_code: "94025",
+                                        state: "CA",
+                                        country: "US"
+                                    },
+                                    summary: {
+                                        subtotal: 75.00,
+                                        shipping_cost: 4.95,
+                                        total_tax: 6.19,
+                                        total_cost: 56.14
+                                    },
+                                    adjustments: [
+                                        { name: "New Customer Discount", amount: 20 },
+                                        { name: "$10 Off Coupon", amount: 10 }
+                                    ]
+                                }
+                            }
+                        }
+                    });
+            } else {
+                msg = new builder.Message(session)
+                    .attachments([
+                        new builder.ReceiptCard(session)
+                            .title(args.model + " " + args.trim)
+                            .items(items)
+                            .facts([
+                                builder.Fact.create(session, session.localizer.gettext(query.kor_en_Checker(session.message.text), "priceReciptTopMenu2"), session.localizer.gettext(query.kor_en_Checker(session.message.text), "priceReciptTopMenu1"))
+                            ])
+                            .total(number_format(total) + session.localizer.gettext(query.kor_en_Checker(session.message.text), "priceReciptCurrencyUnit"))
+                    ]);
+                
+            }
+            session.endDialog(msg);
+
             /*var msg = new builder.Message(session)
                 .attachments([
                     new builder.ReceiptCard(session)
@@ -716,60 +787,8 @@ function create(bot) {
             session.send(msg);
             */
 
-            msg = new builder.Message(session)
-                .sourceEvent({
-                    facebook: {
-                        attachment: {
-                            type: "template",
-                            payload: {
-                                template_type: "receipt",
-                                recipient_name: "Stephane Crozatier",
-                                order_number: "12345678902",
-                                currency: "KRW",
-                                payment_method: "Visa 2345",
-                                order_url: "http://petersapparel.parseapp.com/order?order_id=123456",
-                                timestamp: "1428444852",
-                                elements: [
-                                    {
-                                        title: "Classic White T-Shirt",
-                                        subtitle: "100% Soft and Luxurious Cotton",
-                                        quantity: 2,
-                                        price:1000000,
-                                        currency: "KRW",
-                                        image_url: "http://petersapparel.parseapp.com/img/whiteshirt.png"
-                                    },
-                                    {
-                                        title: "Classic Gray T-Shirt",
-                                        subtitle: "100% Soft and Luxurious Cotton", 
-                                        quantity: 1,
-                                        price: 1250000,
-                                        currency: "KRW",
-                                        image_url: "http://petersapparel.parseapp.com/img/grayshirt.png"
-                                    }
-                                ],
-                                address: {
-                                    street_1: "1 Hacker Way",
-                                    street_2: "",
-                                    city: "Menlo Park",
-                                    postal_code: "94025",
-                                    state: "CA",
-                                    country: "US"
-                                },
-                                summary: {
-                                    subtotal: 75.00,
-                                    shipping_cost: 4.95,
-                                    total_tax: 6.19,
-                                    total_cost: 56.14
-                                },
-                                adjustments: [
-                                    { name: "New Customer Discount", amount: 20 },
-                                    { name: "$10 Off Coupon", amount: 10 }
-                                ]
-                            }
-                        }
-                    }
-                });
-            session.endDialog(msg);
+            
+            
 
             var nextBtn = new builder.Message(session)
                 .attachmentLayout(builder.AttachmentLayout.carousel)
