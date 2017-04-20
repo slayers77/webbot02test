fb5adbc696f3a9663e0f33db7bea10115b4f8b70
Sinook Kang <sokang337@gmail.com>
2017-04-20 14:47:36 +0900

    aa

--- price/kor/priceKor.js
+++ price/kor/priceKor.js
@@ -714,9 +714,9 @@ function create(bot) {
                                 payload: {
                                     template_type: "receipt",
                                     recipient_name: "Stephane Crozatier",
-                                    order_number: "",
+                                    order_number: " ",
                                     currency: "KRW",
-                                    payment_method: "",
+                                    payment_method: " ",
                                     //order_url: "http://petersapparel.parseapp.com/order?order_id=123456",
                                     //timestamp: "",
                                     elements: [
