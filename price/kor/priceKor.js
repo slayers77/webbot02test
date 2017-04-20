65ee14c345d81733496bd9e8a2cd65fdaf6d840d
Sinook Kang <sokang337@gmail.com>
2017-04-20 10:19:00 +0900

    df

--- price/kor/priceKor.js
+++ price/kor/priceKor.js
@@ -714,46 +714,46 @@ function create(bot) {
                                 payload: {
                                     template_type: "receipt",
                                     recipient_name: "Stephane Crozatier",
-                                    order_number: "12345678902",
+                                    //order_number: "12345678902",
                                     currency: "KRW",
                                     payment_method: "Visa 2345",
                                     order_url: "http://petersapparel.parseapp.com/order?order_id=123456",
-                                    timestamp: "1428444852",
+                                    //timestamp: "1428444852",
                                     elements: [
                                         {
-                                            title: "Classic White T-Shirt",
-                                            subtitle: "100% Soft and Luxurious Cotton",
-                                            quantity: 2,
-                                            price: 1000000,
+                                            title: modelNm,
+                                            //subtitle: "100% Soft and Luxurious Cotton",
+                                            quantity: 1,
+                                            price: modelPrice,
                                             currency: "KRW",
-                                            image_url: "http://petersapparel.parseapp.com/img/whiteshirt.png"
+                                            //image_url: "http://petersapparel.parseapp.com/img/whiteshirt.png"
                                         },
                                         {
-                                            title: "Classic Gray T-Shirt",
-                                            subtitle: "100% Soft and Luxurious Cotton",
-                                            quantity: 1,
-                                            price: 1250000,
+                                            title: optionNm,
+                                            //subtitle: "100% Soft and Luxurious Cotton",
+                                            quantity: 2,
+                                            price: optionPrice,
                                             currency: "KRW",
-                                            image_url: "http://petersapparel.parseapp.com/img/grayshirt.png"
+                                            //image_url: "http://petersapparel.parseapp.com/img/grayshirt.png"
                                         }
                                     ],
-                                    address: {
+                                    /*address: {
                                         street_1: "1 Hacker Way",
                                         street_2: "",
                                         city: "Menlo Park",
                                         postal_code: "94025",
                                         state: "CA",
                                         country: "US"
-                                    },
+                                    },*/
                                     summary: {
-                                        subtotal: 75.00,
-                                        shipping_cost: 4.95,
-                                        total_tax: 6.19,
-                                        total_cost: 56.14
+                                        //subtotal: 75.00,
+                                        //shipping_cost: 4.95,
+                                        //total_tax: 6.19,
+                                        total_cost: total
                                     },
                                     adjustments: [
-                                        { name: "New Customer Discount", amount: 20 },
-                                        { name: "$10 Off Coupon", amount: 10 }
+                                        { name: modelNm, amount: modelPrice },
+                                        { name: optionNm, amount: optionPrice }
                                     ]
                                 }
                             }
