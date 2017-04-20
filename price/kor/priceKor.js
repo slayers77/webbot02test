e9e91beee4b28f03dac71596f8cf468cd312cc08
Sinook Kang <sokang337@gmail.com>
2017-04-20 14:25:27 +0900

    aa

--- price/kor/priceKor.js
+++ price/kor/priceKor.js
@@ -714,11 +714,11 @@ function create(bot) {
                                 payload: {
                                     template_type: "receipt",
                                     recipient_name: "Stephane Crozatier",
-                                    //order_number: "12345678902",
+                                    order_number: "12345678902",
                                     currency: "KRW",
                                     payment_method: "Visa 2345",
                                     order_url: "http://petersapparel.parseapp.com/order?order_id=123456",
-                                    //timestamp: "1428444852",
+                                    timestamp: "1428444852",
                                     elements: [
                                         {
                                             title: "Classic White T-Shirt",
