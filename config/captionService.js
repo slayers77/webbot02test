f26995e07925c0810d0f11bb8ae347b7bf6b151b
slayers77 <bobby404@naver.com>
2017-04-27 16:21:39 +0900

    Add files via upload

---
+++ config/captionService.js
@@ -0,0 +1,83 @@
+// The exported functions in this module makes a call to Microsoft Cognitive Service Computer Vision API and return caption
+// description if found. Note: you can do more advanced functionalities like checking
+// the confidence score of the caption. For more info checkout the API documentation:
+// https://www.microsoft.com/cognitive-services/en-us/Computer-Vision-API/documentation/AnalyzeImage
+
+var request = require('request').defaults({ encoding: null });
+
+var VISION_URL = 'https://api.projectoxford.ai/vision/v1.0/analyze/?visualFeatures=Color,Tags,Description&form=BCSIMG&subscription-key=c84d0e1aaaff482b9014a570927bdbf2';
+
+/** 
+ *  Gets the caption of the image from an image stream
+ * @param {stream} stream The stream to an image.
+ * @return {Promise} Promise with caption string if succeeded, error otherwise
+ */
+exports.getCaptionFromStream = function (stream) {
+    return new Promise(
+        function (resolve, reject) {
+            var requestData = {
+                url: VISION_URL,
+                encoding: 'binary',
+                headers: { 'content-type': 'application/octet-stream' }
+            };
+            stream.pipe(request.post(requestData, function (error, response, body) {
+                if (error) {
+                    reject(error);
+                }
+                else if (response.statusCode !== 200) {
+                    reject(body);
+                }
+                else {
+                    console.log(body);
+                    console.log("body ::::::::::::::::::::: " + JSON.parse(body).tags[0].name);
+                    resolve(extractCaption(JSON.parse(body)));
+                }
+            }));
+        }
+    );
+};
+
+/** 
+ * Gets the caption of the image from an image URL
+ * @param {string} url The URL to an image.
+ * @return {Promise} Promise with caption string if succeeded, error otherwise
+ */
+exports.getCaptionFromUrl = function (url) {
+    return new Promise(
+        function (resolve, reject) {
+            var requestData = {
+                url: VISION_URL,
+                json: { 'url': url }
+            };
+            request.post(requestData, function (error, response, body) {
+                if (error) {
+                    reject(error);
+                }
+                else if (response.statusCode !== 200) {
+                    reject(body);
+                }
+                else {
+                    console.log("body ::::::::::::::::::::: " + body);
+                    for (var attr in body) {
+                        console.log(attr + " :::: " + body);
+                    }
+                    resolve(extractCaption(body));
+                }
+            });
+        }
+    );
+};
+
+/**
+ * Extracts the caption description from the response of the Vision API
+ * @param {Object} body Response of the Vision API
+ * @return {string} Description if caption found, null otherwise.
+ */
+function extractCaption(body) {
+    if (body && body.description && body.description.captions && body.description.captions.length) {
+        //return body.description.captions[0].text;
+        return body;
+    }
+
+    return null;
+}
---
+++ config/imageCaption.js
@@ -0,0 +1,103 @@
+// JavaScript source code
+var needle = require('needle'),
+    restify = require('restify'),
+    url = require('url'),
+    validUrl = require('valid-url')
+var date = require('date-utils');
+date = new Date();
+
+//=========================================================
+// Utilities
+//=========================================================
+function hasImageAttachment(session) {
+    return session.message.attachments.length > 0 &&
+        session.message.attachments[0].contentType.indexOf('image') !== -1;
+}
+
+function getImageStreamFromMessage(message) {
+    var headers = {};
+    var attachment = message.attachments[0];
+    if (checkRequiresToken(message)) {
+        // The Skype attachment URLs are secured by JwtToken,
+        // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
+        // https://github.com/Microsoft/BotBuilder/issues/662
+        connector.getAccessToken(function (error, token) {
+            var tok = token;
+            headers['Authorization'] = 'Bearer ' + token;
+            headers['Content-Type'] = 'application/octet-stream';
+
+            return needle.get(attachment.contentUrl, { headers: headers });
+        });
+    }
+
+    headers['Content-Type'] = attachment.contentType;
+    return needle.get(attachment.contentUrl, { headers: headers });
+}
+
+function checkRequiresToken(message) {
+    return message.source === 'skype' || message.source === 'msteams';
+}
+
+/**
+ * Gets the href value in an anchor element.
+ * Skype transforms raw urls to html. Here we extract the href value from the url
+ * @param {string} input Anchor Tag
+ * @return {string} Url matched or null
+ */
+function parseAnchorTag(input) {
+    var match = input.match('^<a href=\"([^\"]*)\">[^<]*</a>$');
+    if (match && match[1]) {
+        return match[1];
+    }
+
+    return null;
+}
+
+//=========================================================
+// Response Handling
+//=========================================================
+function handleSuccessResponse(session, body) {
+    var tag = "";
+    var desctiption = "";
+    var metadata = "";
+    var dominantColor = "";
+    if (body) {
+        console.log("caption :::::::::::::::::::: " + body.color.dominantColors);
+        if (body.description.captions[0].text == 'a red car') {
+            caption = "당신이 선택한 빨간색 입니다.";
+            session.send(caption);
+            session.beginDialog('/korDesignSelectValentineRed', { sendMsg: "image", key: userId, beginTime: date.getTime(), intent: "korDesignSelectValentineRed", tableNm: "insert_history", chanelID: session.message.address.channelId });
+        } else {
+
+            for (var i = 0; i < body.tags.length; i++) {
+
+                tag = tag + body.tags[i].name + " , ";
+
+            }
+
+            desctiption = body.description.captions[0].text;
+            metadata = body.metadata;
+            dominantColor = body.color.dominantColors;
+            
+            session.send("Tags : [ " + tag.substring(0, tag.length - 2) + " ]");
+            session.send("Desctiption : [ " + desctiption + " ]");
+            session.send("dominantColor : [ " + dominantColor + " ]");
+            session.send("metaData : [ Width = " + metadata.width + " Height = " + metadata.height + " Format = " + metadata.format+" ]");
+        }
+        //session.send('I think it\'s ' + caption);
+        
+    }
+    else {
+        session.send('Couldn\'t find a caption for this one');
+    }
+
+}
+
+function handleErrorResponse(session, error) {
+    session.send('Oops! Something went wrong. Try again later.');
+    console.error(error);
+}
+
+module.exports = {
+    hasImageAttachment, getImageStreamFromMessage, checkRequiresToken, parseAnchorTag, handleSuccessResponse, handleErrorResponse
+}
