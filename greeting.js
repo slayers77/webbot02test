de0dd76a25567af63495c1b56e73578636ca8a8d
Sinook Kang <sokang337@gmail.com>
2017-05-10 15:39:16 +0900

    aa

--- greeting.js
+++ greeting.js
@@ -99,7 +99,7 @@ function create(bot) {                                                  // funct
     //초기 출력 화면
     bot.dialog('/init', [
         function (session) {
-            //var botMsg = "안녕하세요. 저는 현대자동차의 그랜저 ig를 소개하는 그랜다이저예요. \n\n 대화중 언제든지'그랜다이저' 라고 입력하면 초기 화면으로 돌아가요.";
+            var botMsg = "안녕하세요. 저는 현대자동차의 그랜저 ig를 소개하는 그랜다이저예요. \n\n 대화중 언제든지'그랜다이저' 라고 입력하면 초기 화면으로 돌아가요.";
             //tts.Synthesize(botMsg);
             console.log("init dialog : " + session.message.address.conversation.id);
             console.log("init dialog : " + session.message.address.id);
