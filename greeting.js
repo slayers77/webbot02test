e308f8b4def7cb172ecf8ea5fea70de645afed82
songhj <songhj@SONGHJ-PC>
2017-05-12 17:33:31 +0900

    name change

--- greeting.js
+++ greeting.js
@@ -99,7 +99,7 @@ function create(bot) {                                                  // funct
     //초기 출력 화면
     bot.dialog('/init', [
         function (session) {
-            var botMsg = "안녕하세요. 저는 현대자동차의 그랜저 ig를 소개하는 그랜다이저예요. \n\n 대화중 언제든지'그랜다이저' 라고 입력하면 초기 화면으로 돌아가요.";
+            var botMsg = "안녕하세요. 저는 현대자동차의 자동차를 소개하는 자동차예요. \n\n 대화중 언제든지'그랜다이저' 라고 입력하면 초기 화면으로 돌아가요.";
             //tts.Synthesize(botMsg);
             console.log("init dialog : " + session.message.address.conversation.id);
             console.log("init dialog : " + session.message.address.id);
@@ -114,7 +114,7 @@ function create(bot) {                                                  // funct
                 .textFormat(builder.TextFormat.xml)
                     .attachments([
                         new builder.VideoCard(session)
-                            .title('그랜다이저11')
+                            .title('자동차11')
                             .autostart(true)
                             .subtitle('Grandizer')
                             .text(botMsg + "\n\n Hi. My name is Grandizer. \n\n At any time, type 'Grandizer' to return to the initial screen. ")
